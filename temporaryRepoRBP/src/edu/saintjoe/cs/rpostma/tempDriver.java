dee119f1327db9367971395dcec647b31ab2d16f
kyled <kyled@localhost>
2016-02-19 08:50:35 -0500

    Minor changes to dialog and array size.

--- temporaryRepoRBP/src/edu/saintjoe/cs/rpostma/tempDriver.java
+++ temporaryRepoRBP/src/edu/saintjoe/cs/rpostma/tempDriver.java
@@ -4,15 +4,15 @@ public class tempDriver {
 
 	public static void main(String[] args) {
 		int array[];
-		int size = 10;//array size
+		int size = 11;//array size
 		
 		array = new int[size];
 		
 		for (int i = 0; i < size; ++i){
 			array[i] = i*i;
-			System.out.println("Array has " + array[i] + " in position " + i);
+			System.out.println("Array has got " + array[i] + " in the position " + i);
 		}
-		System.out.println("Notice a pattern?");
+		System.out.println("Notice a pattern?!?!");
 	
 	}
 	
